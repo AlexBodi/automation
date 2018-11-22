44cfbef9a0d35d28e7358778178c5cc136da8ab1
Alex Bodi <cristian.bodi@ibm.com>
2018-11-22 11:17:19 +0000

    tests added

--- test/locators/locators.js
+++ test/locators/locators.js
@@ -1,7 +1,34 @@
 module.exports = {
  
-  JOB: { xpath: "//job/" },
- 
-
+  JOB:  {
+    xpath: '//a[contains(@href,"/job")]'
+  },
+  IMG: {
+    cssSelector: "img",
+    wantedTextType: ".",
+    wantedTextValue: "//placeimg.com"
   
+  },
+  COMPANY_NAME: {
+    xpath:'//strong[@class="sc-gZMcBi gABsdI"]'
+  },
+
+  LOCATION :{
+    xpath: '//a[contains(@href,"/job")]//span/strong[@class="sc-gZMcBi gABsdI"]/..'
+  },
+  
+  FIRST_JOB: { xpath: '//a[contains(@href,"/job")][1]'},
+
+  JOB_BRIEF: {xpath: '//h2[contains(.,"Job brief")]'},
+  APPLY_BTN: {xpath: '//span[contains(.,"Apply")]'},
+  RESP: { xpath: '//h2[contains(.,"Responsibilities")]' },
+  REQ: {xpath: '//h2[contains(.,"Requirements")]'},
+
+  BACK_BTN: {
+    cssSelector: "span",
+    wantedTextType: ".",
+    wantedTextValue: "Back"
+  },
+  ERROR: {xpath: '//p[contains(.,"Sorry, this url is not pointing anywhere")]'},
+  GO_BACK_BTN: {xpath: '//span[contains(.,"Go back to homepage")]'},
 };
--- test/tests/app_test.js
+++ test/tests/app_test.js
@@ -5,9 +5,36 @@ Feature("HeyJobs testing task");
 
 Scenario("Expect to see a list of 5 jobs, with images, names and locations", I => {
   I.amOnPage("/");
-  I.seeElement(locators.JOB);
+  I.wait(1);
+  I.seeNumberOfElements(locators.JOB, 5);
+  I.seeNumberOfElements(locators.IMG, 5);
+  I.seeNumberOfElements(locators.COMPANY_NAME, 5);
+  I.seeNumberOfElements(locators.LOCATION, 5)
+;});
+Scenario("After users click on a job from the list, they can see job details and can navigate back from a job details page to the job list", I => {
+  I.amOnPage("/");
+  I.wait(1);
+  I.click(locators.FIRST_JOB);
+  I.seeInCurrentUrl("http://localhost:3333/job/");
+  I.seeElement(locators.JOB_BRIEF);
+  I.seeElement(locators.APPLY_BTN);
+  I.seeElement(locators.RESP);
+  I.seeElement(locators.REQ);
+  I.saveScreenshot("job.png");
+  I.seeElement(locators.BACK_BTN);
+  I.take
+  I.click(locators.BACK_BTN);
+  I.seeElement(locators.FIRST_JOB);
 
-  
-  
-  
 });
+
+Scenario("Users see error message when they visit an unknown URL ", I => {
+  I.amOnPage("/foo");
+  I.seeElement(locators.ERROR);
+  I.click(locators.GO_BACK_BTN);
+  I.seeInCurrentUrl("http://localhost:3333");
+
+});
+  
+  
+
